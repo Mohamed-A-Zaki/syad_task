7405e3e0e4c1786123819c59172297d9b305220a
Mohamed Zaki <87452598+Mohamed-A-Zaki@users.noreply.github.com>
2024-03-14 23:04:29 +0200

    statistics section

--- src/components/Statistics/Statistics.tsx
+++ src/components/Statistics/Statistics.tsx
@@ -1,10 +1,76 @@
-export type StatisticsProps = {
-  // props go here
-};
-export default function Statistics(props: StatisticsProps) {
+import images from "@/utils/images";
+import { Container, SimpleGrid, Stack } from "@mantine/core";
+
+export default function Statistics() {
   return (
-    <>
-      <h1>Statistics</h1>
-    </>
+    <section className="py-10 text-white text-center bg-[#C5D8D3]">
+      <Container size={"lg"}>
+        <SimpleGrid cols={{ base: 1, sm: 2, md: 4 }}>
+          <div className="relative group">
+            <img
+              src={images.stats_1}
+              alt=""
+              className="group-hover:scale-110 transition duration-300"
+            />
+            <Stack
+              gap={5}
+              className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+            >
+              <img src={images.stats_icon_1} alt="" />
+              <div className="text-2xl font-semibold">5871.32 كم2</div>
+              <div>المساحات الخضراء</div>
+            </Stack>
+          </div>
+
+          <div className="relative mt-16 group">
+            <img
+              src={images.stats_2}
+              alt=""
+              className="group-hover:scale-110 transition duration-300"
+            />
+            <Stack
+              gap={5}
+              className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+            >
+              <img src={images.stats_icon_2} alt="" />
+              <div className="text-2xl font-semibold">132615.26 كم2</div>
+              <div>المناطق الصحراوية</div>
+            </Stack>
+          </div>
+
+          <div className="relative group">
+            <img
+              src={images.stats_3}
+              alt=""
+              className="group-hover:scale-110 transition duration-300"
+            />
+            <Stack
+              gap={5}
+              className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+            >
+              <img src={images.stats_icon_3} alt="" />
+              <div className="text-2xl font-semibold">174.85 كم2</div>
+              <div>المسطحات المائية</div>
+            </Stack>
+          </div>
+
+          <div className="relative mt-16 group">
+            <img
+              src={images.stats_4}
+              alt=""
+              className="group-hover:scale-110 transition duration-300"
+            />
+            <Stack
+              gap={5}
+              className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+            >
+              <img src={images.stats_icon_4} alt="" />
+              <div className="text-2xl font-semibold">7.02 كم2</div>
+              <div>الأراضي الرطبة</div>
+            </Stack>
+          </div>
+        </SimpleGrid>
+      </Container>
+    </section>
   );
 }
--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,10 +1,19 @@
 import logo_1 from "../assets/logo_1.svg";
 import logo_2 from "../assets/logo_2.png";
 
+import footer_bg from "../assets/footer.png";
 import footer_logo_1 from "../assets/footer_logo_1.svg";
 import footer_logo_2 from "../assets/footer_logo_2.png";
 
-import footer_bg from "../assets/footer.png";
+import stats_1 from "../assets/stats_1.png";
+import stats_2 from "../assets/stats_2.png";
+import stats_3 from "../assets/stats_3.png";
+import stats_4 from "../assets/stats_4.png";
+
+import stats_icon_1 from "../assets/stats_icon_1.svg";
+import stats_icon_2 from "../assets/stats_icon_2.svg";
+import stats_icon_3 from "../assets/stats_icon_3.svg";
+import stats_icon_4 from "../assets/stats_icon_4.svg";
 
 const images = {
   logo_1,
@@ -12,6 +21,14 @@ const images = {
   footer_logo_1,
   footer_logo_2,
   footer_bg,
+  stats_1,
+  stats_2,
+  stats_3,
+  stats_4,
+  stats_icon_1,
+  stats_icon_2,
+  stats_icon_3,
+  stats_icon_4,
 };
 
 export default images;
